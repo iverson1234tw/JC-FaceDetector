9517bd7da192df0aa4fc36430784b8514156df71
iverson1234tw <reno65072013@gmail.com>
2020-06-06 16:20:19 +0800

    Initial Commit

---
+++ FaceDetector/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FaceDetector
+//
+//  Created by iverson1234tw on 2020/6/6.
+//  Copyright © 2020 josh.chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FaceDetector/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  FaceDetector
+//
+//  Created by iverson1234tw on 2020/6/6.
+//  Copyright © 2020 josh.chen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
